bb9b399198b0bfa2a3be89a7f4c2bd3ae4ca9cbc
nladuo <kalen25115@gmail.com>
2016-10-11 18:06:38 +0800

    updated url.

--- backend/application/config/config.php
+++ backend/application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/ustb_class_order';
+$config['base_url']	= 'http://scce.kalen.site';
 
 /*
 |--------------------------------------------------------------------------
